b35c781fd3d6fb314cbb9949e56cd5764bb49471
ldc4 <ldc4@vip.qq.com>
2018-12-28 16:58:21 +0800

    去掉typography.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
   render() {
@@ -13,8 +12,7 @@ class Layout extends React.Component {
       header = (
         <h1
           style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
+            marginBottom: 0,
             marginTop: 0,
           }}
         >
@@ -36,7 +34,7 @@ class Layout extends React.Component {
           style={{
             fontFamily: `Montserrat, sans-serif`,
             marginTop: 0,
-            marginBottom: rhythm(-1),
+            marginBottom: 0,
           }}
         >
           <Link
@@ -57,8 +55,8 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          maxWidth: 1000,
+          padding: '5px 10px',
         }}
       >
         {header}
